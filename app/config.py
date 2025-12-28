ab9ab5dbdd01faa987718f516e5084f14fe4c73e
DINOSH-M <dinooss2005@gmail.com>
2025-12-28 11:02:28 +0530

    commit after delete unnecesssary file

--- app/config.py
+++
@@ -1,4 +0,0 @@
-GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth"
-GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token"
-
-SCOPE = "https://www.googleapis.com/auth/business.manage"
--- app/models.py
+++
@@ -1,3 +0,0 @@
-CLIENTS = {}       # client_id -> ClientOAuthConfig
-TOKENS = {}        # client_id -> token dict
-OAUTH_STATES = {}  # state -> client_id
